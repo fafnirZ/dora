0d57d60123186510c548bc39d8903c9e36b995cc
fafnir <gokurohono@gmail.com>
2025-04-30 23:20:34 +1000

    feat: basic tui working

--- dora/src/main.rs
+++ dora/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Report;
+use dora_tui::app::{App};
+
+fn main() -> Result<(), Report>  {
+    color_eyre::install().expect("didnt install?");
+    let terminal = ratatui::init();
+    let app_result = App::new().run(terminal);
+    ratatui::restore();
+    app_result
 }
