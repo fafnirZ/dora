ac0b21cc998088946407a7f831fa6ec650ef9ddb
fafnir <gokurohono@gmail.com>
2025-05-01 23:44:07 +1000

    feat: using the crossterm backend

--- src/runner.rs
+++ src/runner.rs
@@ -1,7 +1,14 @@
+use std::io::LineWriter;
+use crossterm::{execute, terminal::EnterAlternateScreen};
+use ratatui::{prelude::CrosstermBackend, Terminal};
+
 use crate::app::App;
 
 
 pub fn run_app() {
-    let mut terminal = ratatui::init();
-    App::new().main_loop(&mut terminal);
+    let mut output = std::io::stderr();
+    execute!(output, EnterAlternateScreen).unwrap();
+    let backend = CrosstermBackend::new(LineWriter::new(output));
+    let mut terminal = Terminal::new(backend).unwrap();
+    App::new().main_loop(&mut terminal).unwrap();
 }
