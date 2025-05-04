2acd8e544904b717b270cb2599a8b02ea14f6f97
fafnirz <gokurohono@gmail.com>
2025-05-05 01:05:05 +1000

    chore: better formatting values

--- src/cell.rs
+++ src/cell.rs
@@ -3,7 +3,7 @@ use ratatui::layout::Rect;
 
 pub const HEADER_HEIGHT: u16 = 3;
 pub const CELL_HEIGHT: u16 = 1;
-pub const CELL_WIDTH: u16 = 20;
+pub const CELL_WIDTH: u16 = 15;
 
 
 pub fn get_cell_area(x: u16, y: u16) -> Rect {
