7f1087cadb75e5b4d0884c5f9d6875567aa93cbd
fafnirz <gokurohono@gmail.com>
2025-05-05 01:31:36 +1000

    feat: update styling and fixed up some constant values.

--- src/table/line_number_ui.rs
+++ src/table/line_number_ui.rs
@@ -1,7 +1,7 @@
 use polars::prelude::*;
 use ratatui::prelude::*;
 
-use crate::{any_float, any_int, any_string, any_uint, app::App, cell::{get_cell_area, CELL_HEIGHT, CELL_WIDTH, LINE_NUMBER_CELL_WIDTH}, df::state::CursorFocus, utils::centered_text::{center_text_in_given_area, render_text_centered_in_area}};
+use crate::{any_float, any_int, any_string, any_uint, app::App, cell::{get_cell_area, CELL_HEIGHT, CELL_WIDTH, HEADER_HEIGHT, LINE_NUMBER_CELL_WIDTH}, df::state::CursorFocus, utils::centered_text::{center_text_in_given_area, render_text_centered_in_area}};
 // NOTE: will never add the header to column, since I dont want to be able to navigate to 
 // the header? or maybe treat the header completely differently from a datastructure perspective.
 // imean either way works, its just a choice I gotta deal with in implementation.
@@ -33,7 +33,8 @@ impl StatefulWidget for LineNumberUI {
         // flexbox in html and wanted to do position: absolute for everything...
         let start_x = area.x;
         let start_y= area.y;
-        let end_y = start_y + area.height;
+        let header_offset = HEADER_HEIGHT;
+        let start_y = start_y + header_offset; // need to offset the header height
         
         let [val_offset_start, val_offset_end] = df_state.get_row_view_slice();
 
@@ -43,15 +44,18 @@ impl StatefulWidget for LineNumberUI {
 
             let cell_area = Rect::new(
                 start_x,
-                (start_y + (idx as u16) * CELL_HEIGHT), // column
-                CELL_WIDTH,
+                start_y + (idx as u16) * CELL_HEIGHT, // column
+                LINE_NUMBER_CELL_WIDTH,
                 CELL_HEIGHT,
             );
             let (para, text_area) = center_text_in_given_area(text, cell_area);
-            para.render(
-                text_area,
-                buf,
-            );
+            para
+                .fg(Color::Black)
+                .bg(Color::Gray)
+                .render(
+                    text_area,
+                    buf,
+                );
         }
 } 
     
