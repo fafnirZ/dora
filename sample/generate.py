06d970751af81e964119e43d636546bca361bd8d
fafnir <gokurohono@gmail.com>
2025-05-04 22:23:04 +1000

    feat: adding python script for generating b.csv

---
+++ sample/generate.py
@@ -0,0 +1,11 @@
+import polars as pl
+import random
+
+df = pl.read_csv("a.csv")
+df_len = len(df)
+new_df = df.with_columns(
+    pl.Series(name="random_values", values=[
+        random.randrange(0,9999) for _ in range(df_len)
+    ])
+)
+new_df.write_csv("b.csv")
