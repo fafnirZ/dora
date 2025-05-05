601ee7c95e2e4c83c9d7a3637d369a5eb646f80a
fafnirz <gokurohono@gmail.com>
2025-05-06 08:34:09 +1000

    chore: adding white spaces

--- src/table/table_banner.rs
+++ src/table/table_banner.rs
@@ -37,7 +37,7 @@ impl StatefulWidget for TableBanner {
        let file_name_str = TableBanner::get_file_name(state);
        let search_result_str = TableBanner::get_search_results(state);
 
-       let string_to_render = file_name_str + &search_result_str;
+       let string_to_render = file_name_str + "  " + &search_result_str;
        let text_para = Paragraph::new(string_to_render);
        text_para.render(area, buf);
     }
