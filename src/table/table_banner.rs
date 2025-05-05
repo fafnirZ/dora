bd2d585f56e8cede13cf0d8449b8931c6eab3bec
fafnirz <gokurohono@gmail.com>
2025-05-06 08:33:09 +1000

    feat: adding search results to the table banner

--- src/table/table_banner.rs
+++ src/table/table_banner.rs
@@ -1,7 +1,7 @@
 use ratatui::widgets::{Paragraph, StatefulWidget, Table};
 use ratatui::prelude::*;
 
-use crate::app::App;
+use crate::app::{self, App};
 
 
 
@@ -12,10 +12,22 @@ pub struct TableBanner {}
 impl TableBanner {
     pub fn new() -> Self { Self{} }
 
-    fn get_file_name(state: &<TableBanner as ratatui::prelude::StatefulWidget>::State) -> String {
+    fn get_file_name(state: &<TableBanner as StatefulWidget>::State) -> String {
         let app_state = state;
         let df_state = &app_state.dataframe_state;
         df_state.get_file_name()
+    }
+
+    fn get_search_results(state: &<TableBanner as StatefulWidget>::State) -> String {
+        let app_state = state;
+        let result_state = &app_state.search_result_state;
+        let results = &result_state.result_indices;
+        let result_cursor = match result_state.result_cursor {
+            Some(results) => results,
+            None => return String::new(),
+        };
+
+        return format!("{}/{} results found", result_cursor, results.len());
     }
 }
 
@@ -23,7 +35,10 @@ impl StatefulWidget for TableBanner {
     type State = App;
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer, state: &mut Self::State) {
        let file_name_str = TableBanner::get_file_name(state);
-       let text_para = Paragraph::new(file_name_str);
+       let search_result_str = TableBanner::get_search_results(state);
+
+       let string_to_render = file_name_str + &search_result_str;
+       let text_para = Paragraph::new(string_to_render);
        text_para.render(area, buf);
     }
 }
