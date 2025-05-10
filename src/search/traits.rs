3844d324ccd747532445f54ae47ea306456db034
fafnir <gokurohono@gmail.com>
2025-05-10 10:53:21 +1000

    feat: monomorphization

--- src/search/traits.rs
+++ src/search/traits.rs
@@ -1,3 +1,5 @@
+use super::{approximate_substring_v1::SimpleApproximateSearch, substring::ExactSubstringSearch};
+
 
 pub trait SearchAlgorithm {
     type Result;
@@ -13,8 +15,14 @@ pub trait SearchAlgorithm {
 // NEED TO DO THIS
 // use an enum to wrap all the possible
 // search results
+// so we can actually match it properly
 
 pub enum AnySearchResult {
     SimpleApproximateSearch(Vec<usize>),
     ExactSubstringSearch([usize;2]),
+}
+
+pub enum SearchAlgorithmImplementations {
+    SimpleApproximateSearch(SimpleApproximateSearch),
+    ExactSubstringSearch(ExactSubstringSearch),
 }
