a57a562b08517325e3bce02a8c834c9280001860
fafnir <gokurohono@gmail.com>
2025-05-10 10:01:30 +1000

    feat: Complicated par_find is working nice with the traits and generics

--- src/search/search.rs
+++ src/search/search.rs
@@ -1,25 +1,25 @@
 use rayon::prelude::*;
-
 use super::traits::SearchAlgorithm;
 
-
-// returns vector
-// (index_in_input_vec, slice_inside_input_vec_value)
-pub fn par_find_substring_matches<T: SearchAlgorithm>(
+// returns vector of tuples:
+// (index in input vec, where the string is matched, Matching function's results)
+pub fn par_find_substring_matches<T: SearchAlgorithm + Send + Sync>(
     algorithm: &T,
     input: &Vec<String>,
     substring_to_find: &str,
-) -> T::Result {
+) -> Vec<(usize, T::Result)> 
+where
+    T::Result: Send,
+{
     let case_insensitive = true;
     input
         .par_iter()
         .enumerate()
         .filter_map(|(index, s)| {
             match algorithm.search(substring_to_find, s, case_insensitive) {
-                Some(result) => Some(result),
+                Some(result) => Some((index, result)),
                 None => None,
             }
-            // .map(|match_indices| (index, match_indices))
         })
         .collect()
 }
