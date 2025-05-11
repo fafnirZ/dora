36d3be5b9916cbf12f85802083b781c72ed90bd5
fafnir <gokurohono@gmail.com>
2025-05-11 13:22:42 +1000

    WIP: save

--- src/io/excel/reader.rs
+++ src/io/excel/reader.rs
@@ -4,5 +4,36 @@
 // csv byte buffer
 // then pass the cursor into polars dataframe.
 
-pub struct ExcelReader{}
+use std::io::Cursor;
 
+use polars::error::PolarsError;
+use polars::prelude::*;
+use calamine::open_workbook_auto_from_rs;
+
+use crate::errors::DoraErrors;
+
+pub struct ExcelReader{
+    cursor: Cursor<Vec<u8>>
+}
+
+// this will 'feel' like all the other polars io readers.
+impl ExcelReader {
+    pub fn new(cursor: Cursor<Vec<u8>>) -> Self {
+        Self {
+            cursor: cursor,
+        }
+    }
+
+
+    pub fn finish(&self) -> Result<DataFrame, DoraErrors> {
+        let cal_reader = open_workbook_auto_from_rs(self.cursor)
+            .map_err(|e| DoraErrors::IOError(e.to_string()))?;
+
+        // TODO handle sheet later.
+        // but right now we take first one and convert to dataframe :)
+        
+
+        
+        Ok(())
+    }
+}
