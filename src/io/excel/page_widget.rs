9816a967d5f8cd309eb55e5e3a1c46f7e699e004
fafnir <gokurohono@gmail.com>
2025-05-11 18:08:39 +1000

    feat: initial boilerplate for the visualisation aspect of the sheet selector widget

--- src/io/excel/page_widget.rs
+++ src/io/excel/page_widget.rs
@@ -3,20 +3,45 @@
 // can pick the tabs they wish to select 
 // for the excel reader.
 
-use ratatui::{buffer::Buffer, layout::{Constraint, Layout, Rect}, widgets::StatefulWidget};
+use ratatui::{buffer::Buffer, layout::{Alignment, Constraint, Layout, Rect}, widgets::{Paragraph, StatefulWidget, Widget}};
 
 pub struct ExcelSheetSelectorWidgetState {
-    sheet_names: Vec<String>,
+    sheet_names: Option<Vec<String>>,
     cursor: u16,
 }
+
+// const ELEMENT_WIDTH: u16 = 10;
+const ELEMENT_HEIGHT: u16 = 1;
 
 pub struct ExcelSheetSelectorPage {}
 
 impl ExcelSheetSelectorPage {
     pub fn new() -> Self { Self {} }
 
-    fn render_widget(&self, area: Rect, buf: &mut Buffer, state: &mut <ExcelSheetSelectorPage as StatefulWidget>::State) {
+    fn render_element(&self, 
+        sheet_name: &str,
+        area: Rect, buf: &mut Buffer
+    ) {
+        Paragraph::new(sheet_name)
+            .alignment(Alignment::Center)
+            .render(area, buf);
+    }
 
+    fn render_widget(&self, area: Rect, buf: &mut Buffer, state: &mut <ExcelSheetSelectorPage as StatefulWidget>::State) {
+        let start_x = area.x;
+        let start_y = area.y;
+        let sheet_names = 
+            state
+            .sheet_names
+            .as_ref()
+            .expect("excel file doesnt have sheet names? is this possible?");
+
+        for (idx, sheet_name) in sheet_names.iter().enumerate() {
+            let curr_y = start_y + (idx as u16)*ELEMENT_HEIGHT;
+            // fills width to the size of the element.
+            let area = Rect::new(start_x, curr_y, area.width, ELEMENT_HEIGHT);
+            self.render_element(sheet_name, area, buf);
+        }
     }
 }
 
@@ -43,7 +68,7 @@ impl StatefulWidget for ExcelSheetSelectorPage {
             _,
         ] = Layout::horizontal([
             Constraint::Fill(1),
-            Constraint::Percentage(50),
+            Constraint::Percentage(10),
             Constraint::Fill(1),
         ]).areas(main);
 
