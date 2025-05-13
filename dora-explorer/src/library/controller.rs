ab473dd90f5fa1336982de700fa5615e98cd0a57
fafnir <gokurohono@gmail.com>
2025-05-14 00:49:01 +1000

    feat: handle up and down navigation

--- dora-explorer/src/library/controller.rs
+++ dora-explorer/src/library/controller.rs
@@ -11,7 +11,26 @@ pub struct Controller {}
 impl Controller {
     // this function mutates the app state
     pub fn perform_actions(control: &Control, state: &mut ExplorerState) {
-      
+        match control {
+            Control::ScrollUp => {
+                let cursor_pos = &state.cursor_y;
+                if *cursor_pos == 0 {
+                    return;
+                } else {
+                    state.cursor_y -= 1;
+                }
+            }
+            Control::ScrollDown => {
+                let cursor_pos = &state.cursor_y;
+                let num_dents = &state.dents.len();
+                if *cursor_pos == (*num_dents as u16)-1 {
+                    return;
+                } else {
+                    state.cursor_y += 1;
+                }
+            }
+            _ => {}
+        }
     }
 
 }
