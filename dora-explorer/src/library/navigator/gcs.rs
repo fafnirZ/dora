2002d4957650be36fd9706d6b70051e2b4b77ea6
fafnir <gokurohono@gmail.com>
2025-05-15 00:29:18 +1000

    feat: starting boilerplate code for gcs navigator

--- dora-explorer/src/library/navigator/gcs.rs
+++ dora-explorer/src/library/navigator/gcs.rs
@@ -1,20 +1,47 @@
-// use crate::library::{errors::ExplorerError, ExplorerState};
+use std::{any::Any, f32::consts::E, path::{Path, PathBuf}};
 
-// use super::types::DEnt;
+use crate::library::{errors::ExplorerError, ExplorerState};
 
-
-
-
-// // pub fn go_out_of_folder(state: &mut ExplorerState) -> Result<(), ExplorerError> {
-// // }
-
-// // pub fn go_into_folder(state: &mut ExplorerState) -> Result<(), ExplorerError> {
-// // }
-
-// // fn refresh_d_ents(state: &mut ExplorerState) -> Result<(), ExplorerError> {
-// // }
+use super::{traits::{AnyPath, Navigator}, types::{DEnt, FileType}};
 
 
+pub struct GCSNavigator{}
 
-// pub fn getdents_from_path(path: &Path) -> Result<Vec<DEnt>, ExplorerError> {
-// }
+
+impl Navigator for GCSNavigator {
+
+    fn go_out_of_folder(state: &mut ExplorerState) -> Result<(), ExplorerError> {
+
+        if let AnyPath::GSPath(cwd) = &state.cwd {
+            Ok(())
+        } else {
+            return Err(ExplorerError::NotARemotePath("Expected a local path.".to_string()))
+        }
+        
+    }
+
+    fn go_into_folder(state: &mut ExplorerState) -> Result<(), ExplorerError> {
+        
+                if let AnyPath::GSPath(cwd) = &state.cwd {
+            Ok(())
+        } else {
+            return Err(ExplorerError::NotARemotePath("Expected a local path.".to_string()))
+        }
+    }
+
+    fn refresh_d_ents(state: &mut ExplorerState) -> Result<(), ExplorerError> {
+        if let AnyPath::GSPath(cwd) = &state.cwd {
+            Ok(())
+        } else {
+            return Err(ExplorerError::NotARemotePath("Expected a local path.".to_string()))
+        }
+    }
+}
+
+
+
+// local path implementation
+pub fn getdents_from_path(path: &Path) -> Result<Vec<DEnt>, ExplorerError>{
+
+    Ok(())
+}
