03641334168633d98715344315e96f229440783e
fafnir <gokurohono@gmail.com>
2025-05-20 01:00:17 +1000

    feat: logic is sound in local mode

--- dora-explorer/src/library/navigator/local.rs
+++ dora-explorer/src/library/navigator/local.rs
@@ -49,7 +49,17 @@ impl Navigator for LocalNavigator {
         if let AnyPath::LocalPath(cwd) = &state.cwd {
             let cursor_pos = *&state.cursor_y;
             let absolute_pos = &state.view_slice[0] + cursor_pos;
-            if let AnyPath::LocalPath(selected_dir) = &state.dents[absolute_pos as usize].path {
+            let dents = {
+                if state.dents_filterview.is_some() {
+                    &state
+                        .dents_filterview
+                        .as_ref()
+                        .unwrap()
+                }else {
+                    &state.dents
+                }
+            };
+            if let AnyPath::LocalPath(selected_dir) = &dents[absolute_pos as usize].path {
                 let new_path = cwd.join(selected_dir);
 
                 // check if the new path is a dir 
@@ -91,6 +101,7 @@ impl Navigator for LocalNavigator {
                     getdents_from_path(cwd)?
                 }
             };
+            state.dents_filterview = None;
             Ok(())
         } else {
             return Err(ExplorerError::NotALocalPath("Expected a local path.".to_string()))
