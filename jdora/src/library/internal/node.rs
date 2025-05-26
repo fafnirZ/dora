22292436e45b3fb717904a615e4ca1b7411881ca
Jacky <gokurohono@gmail.com>
2025-05-26 22:07:03 +1000

    chore: updates

--- jdora/src/library/internal/node.rs
+++ jdora/src/library/internal/node.rs
@@ -2,7 +2,7 @@ use std::primitive;
 
 use serde_json::{Map, Value};
 
-use super::node_path::NodePath;
+use super::node_path::{self, NodePath, NodePathKey};
 
 
 const INDENT_SIZE: u16 = 4;
@@ -12,8 +12,9 @@ const INDENT_SIZE: u16 = 4;
 
 #[derive(Debug)]
 pub struct Node {
-    serde_node: Value, // forwards to serde node
-    indent_level: u16,
+    // serde_node: Value, // forwards to serde node
+    pub node_path: NodePath,
+    pub indent_level: u16,
     pub primitives: Vec<(String, Value)>, // primitive attributes
     // NOTE i understand I don't handle lists well....at all right now...
     pub children: Vec<(String, Node)>,
@@ -23,22 +24,23 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn new(val: Value, indent_level: u16) -> Option<Self> {
+    pub fn new(val: Value, node_path: NodePath) -> Option<Self> {
         if !matches!(val, Value::Object(_)) {
             return None
         }
 
-        let (primitives, children) = Node::parse(&val, indent_level);
+        let (primitives, children) = Node::parse(&val, &node_path);
         Some(Self{
-            serde_node: val,
-            indent_level: indent_level,
+            // serde_node: val,
+            node_path: node_path.clone(),
+            indent_level: (node_path.path.len() as u16),
             primitives: primitives,
             children: children,
             hidden_children: Vec::new(),
         })
     }
 
-    pub fn parse(serde_node: &Value, indent_level: u16) -> (
+    pub fn parse(serde_node: &Value, node_path: &NodePath) -> (
         Vec<(String, Value)>, // primitives
         Vec<(String, Node)>,  // nested nodes 
     ){
@@ -53,7 +55,12 @@ impl Node {
                         children.push(
                             (
                                 key.to_string(), 
-                                Node::new(val.clone(), indent_level+1).unwrap()
+                                Node::new(
+                                    val.clone(), 
+                                    node_path.push_and_clone(
+                                        NodePathKey::DictKey(key.to_string())
+                                    )
+                                ).unwrap()
                             )
                         )
                     }
@@ -73,7 +80,10 @@ impl Node {
         }
     }
 
-    pub fn pprint(&self) -> String {
+    ///
+    /// for every line which will be sent to pprint
+    /// associate the NodePath associated with this line.
+    pub fn get_structures(&self) -> Vec<(String, NodePath)> {
         let mut result = String::new();
 
 
@@ -107,6 +117,42 @@ impl Node {
         
         result
     }
+    // pub fn pprint(&self) -> String {
+    //     let mut result = String::new();
+
+
+    //     // open brace
+    //     // result += &self.num_spaces(self.indent_level*INDENT_SIZE);
+    //     result += "{\n";
+
+    //     // print primitives first
+    //     for prim_attr in self.primitives.iter() {
+    //         let (key, val) = prim_attr.clone();
+    //         result += &self.num_spaces((self.indent_level+1)*INDENT_SIZE);
+    //         result += &format!("\"{}\":{}", key, val.to_string());
+    //         result += ",\n"
+    //     }
+
+    //     // print children
+    //     for (idx, child ) in self.children.iter().enumerate() {
+    //         let (key, chld) = child;
+    //         result += &self.num_spaces((self.indent_level+1)*INDENT_SIZE);
+    //         result += &format!("\"{}\":", key);
+    //         if self.hidden_children.contains(&(idx as u16)) {
+    //             result += &format!("<collapsed>({} lines)\n", chld.calculate_num_lines());
+    //         } else {
+    //             result += &chld.pprint();
+    //         }
+    //     }
+
+    //     // print closing bracket
+    //     result += &self.num_spaces(self.indent_level*INDENT_SIZE);
+    //     result += "}\n";
+        
+    //     result
+    // }
+
+
 
     fn num_spaces(&self, n: u16) -> String {
         " ".repeat(n as usize).to_string()
@@ -127,43 +173,43 @@ impl Node {
         bracket_lines+primitive_len+children_len
     }
     
-    // TODO: need to think deeply
-    // about the maths and calculations
-    // for this function and the calculate_num_lines_function
-    pub fn build_children_line_boundaries(&self, current_line_pos: u16, curr_node_path: &NodePath) -> Vec<(NodePath, (u16,u16))>{
-        if self.children.len() == 0 { // is leaf node
-            let mut v = Vec::new();
-            v.push(
-                (
-                    curr_node_path.clone(), 
-                    (current_line_pos, current_line_pos+self.calculate_num_lines()-1)
-                )
-            );
-            return v;
-        }
+    // // TODO: need to think deeply
+    // // about the maths and calculations
+    // // for this function and the calculate_num_lines_function
+    // pub fn build_children_line_boundaries(&self, current_line_pos: u16, curr_node_path: &NodePath) -> Vec<(NodePath, (u16,u16))>{
+    //     if self.children.len() == 0 { // is leaf node
+    //         let mut v = Vec::new();
+    //         v.push(
+    //             (
+    //                 curr_node_path.clone(), 
+    //                 (current_line_pos, current_line_pos+self.calculate_num_lines()-1)
+    //             )
+    //         );
+    //         return v;
+    //     }
 
-        let mut v = Vec::new();
-        let open_bracket_offset = 1_u16;
-        let mut line_pos_offset = current_line_pos 
-            + (self.primitives.len() as u16)
-            + open_bracket_offset;
+    //     let mut v = Vec::new();
+    //     let open_bracket_offset = 1_u16;
+    //     let mut line_pos_offset = current_line_pos 
+    //         + (self.primitives.len() as u16)
+    //         + open_bracket_offset;
 
-        for (idx, (_, child)) in self.children.iter().enumerate() {
-            let new_node_path = curr_node_path.push_and_clone(idx);
-            let child_boundaries = child.build_children_line_boundaries(
-                line_pos_offset, // update line position offsets
-                &new_node_path
-            ); 
-            for child_boundary in child_boundaries {
-                v.push(child_boundary);
-            }
-            let child_total_len = child.calculate_num_lines();
-            line_pos_offset += child_total_len;
-        }
-        v.push(
-            (curr_node_path.clone(), (current_line_pos, current_line_pos+(self.calculate_num_lines()-1)))
-        );
-        return v;
-    }
+    //     for (idx, (_, child)) in self.children.iter().enumerate() {
+    //         let new_node_path = curr_node_path.push_and_clone(idx);
+    //         let child_boundaries = child.build_children_line_boundaries(
+    //             line_pos_offset, // update line position offsets
+    //             &new_node_path
+    //         ); 
+    //         for child_boundary in child_boundaries {
+    //             v.push(child_boundary);
+    //         }
+    //         let child_total_len = child.calculate_num_lines();
+    //         line_pos_offset += child_total_len;
+    //     }
+    //     v.push(
+    //         (curr_node_path.clone(), (current_line_pos, current_line_pos+(self.calculate_num_lines()-1)))
+    //     );
+    //     return v;
+    // }
 }
 
