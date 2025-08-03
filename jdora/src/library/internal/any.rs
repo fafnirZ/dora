815e5de29f30140a1d8c9c034a590cf896c96eb5
Jacky <gokurohono@gmail.com>
2025-08-03 22:14:36 +1000

    chore: more re-org

--- jdora/src/library/internal/any.rs
+++ jdora/src/library/internal/any.rs
@@ -9,7 +9,63 @@ pub enum AnyNode {
     NestedNode(Node),
 }
 
+
+#[derive(Debug)]
+pub struct ParsedNodePacket {
+    key: String, // key from json
+    node: AnyNode, // Node with type information
+}
+
+
 impl AnyNode {
+    pub fn parse(serde_node: &Value, node_path: &NodePath) -> Vec<ParsedNodePacket> {
+        if let Value::Object(map) = &serde_node {
+            let mut children: Vec<ParsedNodePacket> = Vec::new();
+
+            for (key, val) in map.iter() {
+                match val {
+                    Value::Object(_) => {
+                        children.push(
+                            ParsedNodePacket{
+                                key: key.to_string(), 
+                                node: AnyNode::NestedNode(Node::new(
+                                    val.clone(), 
+                                    node_path.push_and_clone(
+                                        NodePathKey::DictKey(key.to_string())
+                                    ),
+                                )),
+                            }
+                        )
+                    }
+                    Value::Array(_) => {
+                        // TODO 
+                        //     ParsedNodePacket{
+                        //         key: key.to_string(), 
+                        //         node: AnyNodeNode::new(
+                        //             val.clone(), 
+                        //             node_path.push_and_clone(
+                        //                 NodePathKey::DictKey(key.to_string())
+                        //             ),
+                        //             NodeType::IterableNodes,
+                        //         ),
+                        //     }
+                        // ) 
+                    }
+                    _ => {
+                        children.push(
+                            ParsedNodePacket{
+                                key: key.to_string(), 
+                                node: AnyNode::PrimitiveNode(val.clone()),
+                            }
+                        )
+                    }
+                }
+            }
+            return children
+        } else {
+            panic!("parse failed? node is not an object")
+        }
+    }
     pub fn calculate_num_lines(&self) -> u16 {
         match self {
             AnyNode::PrimitiveNode(_) => {
